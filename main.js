758e5f7776273713851d0829ab6cd0b86961c49b
Amy Knudson <amylknudson@gmail.com>
2019-03-25 16:05:44 -0600

    drop down works and menu sticks to the top

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 let dropDown = document.getElementsByClassName("drop-down")[0];
 
+function scrollTo(e){
+    let page = document.getElementById(e.toElement.innerText)
+    page.scrollIntoView()
+}
 
 function hamburgerX(x) {
     x.classList.toggle("change");
     dropDown.classList.toggle("show");
 }
+
+let navLinks = document.querySelectorAll(".go-to")
+
+navLinks.forEach(element => {
+    element.addEventListener("click", scrollTo)
+})
