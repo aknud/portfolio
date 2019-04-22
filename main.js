dd6490765ed65c414fbda4ca9b7546dee88567a2
Amy Knudson <amylknudson@gmail.com>
2019-04-22 00:09:24 -0600

    added typing effects

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let dropDown = document.getElementsByClassName("drop-down")[0];
 let burger = document.getElementsByClassName("hamburger")[0];
 
 function scrollTo(e){
-    let page = document.getElementById(e.toElement.innerText)
-    hamburgerX(burger)
-    page.scrollIntoView()
+    let page = document.getElementById(e.toElement.innerText);
+    hamburgerX(burger);
+    page.scrollIntoView();
 }
 
 function hamburgerX(x) {
@@ -12,8 +12,66 @@ function hamburgerX(x) {
     dropDown.classList.toggle("show");
 }
 
-let navLinks = document.querySelectorAll(".go-to")
+// Gets all the items in the menu, loops over them and adds an on click event listener so that when they are clicked the dom "scrolls" to the selected section view.
+let navLinks = document.querySelectorAll(".go-to");
 
 navLinks.forEach(element => {
     element.addEventListener("click", scrollTo)
-})
+});
+
+//Typing effect
+const TxtType = function(el, toRotate, pauseTime){
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.pauseTime = parseInt(pauseTime, 10) || 2000;
+    this.txt = "";
+    this.cursor();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.cursor = function(){
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if(this.isDeleting){
+        this.txt = fullTxt.substring(0, this.txt.length -1)
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1)
+    }
+
+    this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if(this.isDeleting){
+        delta /= 2;
+    }
+
+    //checks to see if the word is finished typing
+    //if true sets isDelete to true
+    if(!this.isDeleting && this.txt === fullTxt){
+        delta = this.pauseTime;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === ""){
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(()=> {
+        that.cursor()
+    }, delta);
+}
+
+window.onload = function(){
+    let elements = document.getElementsByClassName("typewrite");
+    for(let i = 0; i < elements.length; i++){
+        let toRotate = elements[i].getAttribute("data-rotate");
+        let period = elements[i].getAttribute("data-period");
+        if(toRotate){
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
